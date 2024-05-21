50c5cb43bad2412e182c1e5a97555559ed172fab
SparroWServ <133554592+SparroWServ@users.noreply.github.com>
2024-05-20 21:48:31 -0400

    Update entry.sh

--- etc/entry.sh
+++ etc/entry.sh
@@ -4,10 +4,10 @@
 /home/steam/steamcmd/steamcmd.sh \
     +force_install_dir /home/steam/kf2-dedicated \
     +login anonymous \
-     +app_update 232130 \
+    +app_update 232130 \
     +exit
 
 # start the server
-/home/steam/kf2-dedicated/Binaries/Win64/KFGameSteamServer.bin.x86_64 'KF-BurningParis'
+/home/steam/kf2-dedicated/Binaries/Win64/KFGameSteamServer.bin.x86_64 'KF-BurningParis' -Port=7779 -QueryPort=27017 -WebAdminPort=8080
 
 exit 0
